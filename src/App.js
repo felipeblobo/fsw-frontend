4984fcd0e454c1742c9d7d78162210e7296e7801
Felipe Lobo <felipeblobo@gmail.com>
2021-03-25 22:25:25 -0300

    Iniciando Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import "antd/dist/antd.css";
+
+const {Header, Content, Footer} = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo">
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">Home</Menu.Item>
+            </Menu>
+          </div>
+        </Header>
+        <Content style={{padding: '0 50px'}}>
+          <div className="site-layout-content">
+            Opa
+          </div>
+        </Content>
+        <Footer style={{textAlign: 'center'}}>
+          Vacina Para Todos
+        </Footer>
+      </Layout>
     </div>
   );
 }
---
+++ src/services/PessoaDataService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/';
+
+class PessoaDataService {
+    retornaTodasPessoas(){
+        return axios.get(`${API_URL}pessoas`);
+    }
+
+    atualizarPessoa(pessoa, codigo) {
+        return axios.put(`{API_URL}pessoas/${codigo}`, pessoa);
+    }
+}
+
+export default PessoaDataService;
